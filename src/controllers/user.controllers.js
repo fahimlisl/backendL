5a4443477aca68541f366340162567596a3750c2
fahimlisl <developerfahim134@gmail.com>
2025-11-19 02:19:12 +0530

    whole is done , but many tasks are left

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -3,9 +3,8 @@ import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.models.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-// import { AsyncLocalStorage } from "async_hooks";
-
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
 // careting method for generatiing access and refresh token sepearetly
 const generateAccessAndRefreshTokens = async (userId) => {
@@ -213,7 +212,219 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
   );
 });
 
-export { registerUser, loginUser, logOutUser ,refreshAccessToken};
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+  const { oldPassword, newPassword } = req.body;
+  const user = await User.findById(req.user?._id);
+  const isPasswordValid = await user.isPasswordCorrect(oldPassword);
+  if (!isPasswordValid) {
+    throw new ApiError(401, "invalid old password");
+  }
+
+  user.password = newPassword;
+  await user.save({ validateBeforeSave: false });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "password changed successyflly"));
+});
+
+const getCurrentUser = asyncHandler(async (req, res) => {
+  return res
+    .status(200)
+    .json(200, req.user, "current user fetched successfully");
+});
+
+const updateAccountDeatils = asyncHandler(async (req, res) => {
+  const { fullName, email } = req.body;
+
+  if (!fullName || !email) {
+    throw new ApiError(400, "all fieldds are requied");
+  }
+  const user = User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: {
+        fullName,
+        email,
+      },
+    },
+    { new: true }
+  ).select("-password");
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user, "ACcount deatils updated susscusssfully"));
+});
+
+const updateUserAvatar = asyncHandler(async (req, res) => {
+  // const avatarLocalPath = await req.file?.avatar[0]?.path; // it will be 'file' not 'files' due to we are accepting only one file not multiple files ,also we need to edit the rouer part for it
+  const avatarLocalPath = req.files?.path;
+  if (!avatarLocalPath) {
+    throw new ApiError(
+      400,
+      "avatarLocal path is missing, line 267 user controller"
+    );
+  }
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  if (!avatar.url) {
+    throw new ApiError(
+      400,
+      "error while uploading on error , got on line 271 on user controller"
+    );
+  }
+
+  // user.avatar = avatar
+  // user.save({validateBeforeSave:false})
+
+  const user = User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: {
+        avatar: avatar.url,
+      },
+    },
+    { new: true }
+  ).select("-password");
+
+  return res
+  .status(200)
+  .json(new ApiResponse(200,user,"avatar image update succsully"))
+});
+
+const getUserChannelProfile = asyncHandler( async( req,res) => {
+  const {username} = req.params
+   
+  if (!username?.trim()) {
+    throw new ApiError(400,"usernmae is missing")
+  }
+
+  const channel = await User.aggregate([
+    {
+      $match:{
+        username: username?.toLowerCase()
+      }
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscribers"
+      },
+    },
+    {
+      $lookup:{
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "subscribedTo"
+      }
+    },
+    {
+      $addFields:{
+        subscribersCount:{
+          $size: "$subscribers"
+        },
+        channelsSubscribedToCount: {
+          $size: "$subscribedTo"
+        },
+        isSubscribed:{
+          $cond:{
+            if: {$in: [req.user?._id, "$subscribers.subscriber"]},
+            then: true,
+            else: false,
+          }
+        }
+      }
+    },
+    {
+      $project : {
+        fullName: 1,
+        username: 1,
+        subscribersCount:1,
+        isSubscribed: 1,
+        channelsSubscribedToCount: 1,
+        avatar: 1,
+        coverImage: 1,
+        email: 1
+
+      }
+    }
+  ])
+
+  if (!channel?.length) {
+    throw new ApiError(404,"cahnnel does not exist")
+  }
+
+  return res
+  .status(200)
+  .json(new ApiResponse(200, channel[0],"user channel fetched successfully"))
+})
+
+const getWatchHistory = asyncHandler( async(req,res) => {
+  const uesr = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user._id)
+      }
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as:"watchHistory",
+        pipeline: [
+          {
+            $lookup:{
+              from:"users",
+              localField:"owner",
+              foreignField:"_id",
+              as:"owner",
+              pipeline:[
+                {
+                  $project:{
+                    fullName: 1,
+                    username:1,
+                    avatar:1
+                  }
+                }
+              ]
+            }
+          },
+          {
+            $addFields:{
+              owner:{
+                $first:"$owner"
+              }
+            }
+          }
+        ]
+      }
+    }
+  ])
+
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(200,
+      user[0].watchHistory,
+      "watch history fetched successfully"
+    )
+  )
+})
+
+export {
+  registerUser,
+  loginUser,
+  logOutUser,
+  refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDeatils,
+  updateUserAvatar,
+  getUserChannelProfile
+};
 
 // whole procedure of logic backend
 
@@ -226,54 +437,3 @@ export { registerUser, loginUser, logOutUser ,refreshAccessToken};
 // remove password and refresh token field from response
 // chec for user creation
 // return response back
-
-// import { asyncHandler } from "../utils/asyncHandler.js";
-// import { uploadOnCloudinary } from "../utils/cloudinary.js";
-// import { ApiResponse } from "../utils/ApiResponse.js";
-// import { User } from "../models/user.models.js";
-// import { ApiError } from "../utils/ApiError.js";
-
-// const registerUser = asyncHandler(async (req, res) => {
-//   const { username, email, password, fullName } = req.body;
-
-//   // validaton
-
-//   if (
-//     [username, email, fullName, password].some((field) => field?.trim() === "")
-//   ) {
-//     throw new ApiError(500, "require every field to filled on");
-//   }
-
-//   const existedUser = await User.findOne({
-//     $or: [{ email }, { username }],
-//   });
-
-//   if (existedUser) {
-//     console.log(`got error user exits already`);
-
-//     throw new ApiError(408, "user already exists");
-//   }
-
-//   const avatarLocalPath = req.files?.avatar[0]?.path;
-//   const coverIamgeLocalPath = req.files?.coverImage[0]?.path;
-
-//   if (!avatarLocalPath) {
-//     throw new ApiError(500, "avatar is required");
-//   }
-
-//   const avatar = await uploadOnCloudinary(avatarLocalPath);
-//   const coverImage = await uploadOnCloudinary(coverIamgeLocalPath);
-//   const user = await User.create({
-//     username,
-//     fullName,
-//     avatar: avatar.url,
-//     coverImage: coverImage?.url || "",
-//     email,
-//     password,
-//   });
-//   return res
-//     .status(201)
-//     .json(new ApiResponse(200, user, "user created successsfully "));
-// });
-
-// export { registerUser };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logOutUser, refreshAccessToken, registerUser } from "../controllers/user.controllers.js";
+import { changeCurrentPassword, loginUser, logOutUser, refreshAccessToken, registerUser } from "../controllers/user.controllers.js";
 import {upload} from "../middlewares/multer.middlewares.js"
 
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
@@ -20,9 +20,11 @@ router.route("/register").post(
     registerUser)
 
 router.route("/login").post(loginUser)
+router.route("/refresh-token").post(refreshAccessToken)
 
 // secured routes
 router.route("/logout").post(verifyJWT,logOutUser)
-router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT,changeCurrentPassword)
+
 
 export default router
